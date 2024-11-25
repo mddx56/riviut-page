1527d378de390db2daa62410e60851cd53a992a0
mddx56 <waltermj2012@gmail.com>
2024-11-25 14:35:36 -0400

    add metatags

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -5,6 +5,36 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="clinica de santa cruz" />
       <link rel="icon" href="/images/favicon.ico" />
+
+      <meta name="title" content="Riviut" />
+      <meta
+        name="description"
+        content="Clinica especializada en salud renal Reviut."
+      />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://riviut.vercel.app/" />
+      <meta property="og:title" content="Riviut" />
+      <meta
+        property="og:description"
+        content="Clinica especializada en salud renal Reviut."
+      />
+      <meta
+        property="og:image"
+        content="https://metatags.io/images/meta-tags.png"
+      />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://riviut.vercel.app/" />
+      <meta property="twitter:title" content="Riviut" />
+      <meta
+        property="twitter:description"
+        content="Clinica especializada en salud renal Reviut."
+      />
+      <meta
+        property="twitter:image"
+        content="https://metatags.io/images/meta-tags.png"
+      />
     </>
   );
 }
